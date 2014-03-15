206372a5db90d37202f4d8d725c9337d6e3fefa6
jivetestuser <jivetestuser@yahoo.com>
2014-03-15 10:33:47 -0700

    Added a title helper and stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,16 +3,22 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+
     it "should have the right title" do
     get 'home'
-    response.body.should include 'Ruby on Rails Tutorial Sample App | Home</title>'
+    response.body.should include "#{@base_title} | Home</title>"
     #response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
     end
+
     it "should have a non-blank body" do
       get 'home'
       response.body.should_not =~ /<body>\s*<\/body>/
@@ -24,11 +30,12 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
     it "should have the right title" do
     get 'contact'
-    response.body.should include 'Ruby on Rails Tutorial Sample App | Contact</title>'
+    response.body.should include "#{@base_title} | Contact</title>"
     #response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
-  end
+    end
   end
 
   describe "GET 'about'" do
@@ -36,11 +43,12 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+
     it "should have the right title" do
     get 'about'
-    response.body.should include 'Ruby on Rails Tutorial Sample App | About</title>'
+    response.body.should include "#{@base_title} | About</title>"
     #response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
-  end
+    end
   end
 
 end
