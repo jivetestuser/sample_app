7ebfd0eea4d0e75ea5beb657999f5aba21a42162
jivetestuser <jivetestuser@yahoo.com>
2014-03-16 23:54:20 -0700

    Added user controller and layout link test

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -3,22 +3,41 @@ require 'spec_helper'
 describe "LayoutLinks" do
 it "should have a Home page at '/'" do
   get '/'
-  response.body.should include "title | Home</title>"
+  response.should have_selector('title', :content => "Home")
 end
 
 it "should have a Contact page at '/contact'" do
   get '/contact'
-  response.body.should include "title | Contact</title>"
+  response.should have_selector('title', :content => "Contact")
 end
 
 it "should have a About page at '/about'" do
   get '/about'
-  response.body.should include "title | About</title>"
+  response.should have_selector('title', :content => "About")
 end
 
 it "should have a Help page at '/'" do
   get '/help'
-  response.body.should include "title | Help</title>"
+  response.should have_selector('title', :content => "Help")
 end
 
+it "should have a sign up page at '/signup'" do
+  get '/signup'
+  #response.body.should include "Ruby on Rails Tutorial Sample App | Sign up</title>"
+  response.should have_selector('title', :content => "Sign up")
+end
+
+it "should have right links on the layout" do
+  visit root_path
+  response.should have_selector('title', :content => "Home")
+  click_link "About"
+  response.should have_selector('title', :content => "About")
+  click_link "Contact"
+  response.should have_selector('title', :content => "Contact")
+  click_link "Home"
+  response.should have_selector('title', :content => "Home")
+  click_link "Sign up now!"
+  response.should have_selector('title', :content => "Sign up")
+  response.should have_selector('a[href="/"]>img')
+end
 end
